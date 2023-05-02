8986d554c3f0d6e43cf2a66cd2b09eb912562509
Paul Hayes <fofr@users.noreply.github.com>
2023-05-02 21:39:19 +0100

    Improve handling of float arguments

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN
 })
 
-function parseArgs (args) {
+const convertValue = (value) => {
+  if (!isNaN(value) && value.trim() !== '') {
+    const parsedValue = parseFloat(value)
+    return Number.isInteger(parsedValue) ? parseInt(value) : parsedValue
+  }
+  return value
+}
+
+const parseArgs = (args) => {
   const options = {}
   let unnamedArgIndex = 0
   for (let i = 0; i < args.length; i++) {
     if (args[i].startsWith('--')) {
-      options[args[i].slice(2)] = args[++i]
+      options[args[i].slice(2)] = convertValue(args[++i])
     } else {
-      options[`unnamedArg${unnamedArgIndex++}`] = args[i]
+      options[`unnamedArg${unnamedArgIndex++}`] = convertValue(args[i])
     }
   }
   return options
