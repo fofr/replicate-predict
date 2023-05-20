f7da391eeb765758039a6f3bec4248d6afd91fda
Paul Hayes <fofr@users.noreply.github.com>
2023-05-20 22:15:35 +0100

    Use tortoise for talking heads flow

--- workflows/talking-head.js
+++ workflows/talking-head.js
@@ -1,4 +1,4 @@
-import Bark from '../models/bark.js'
+import Tortoise from '../models/tortoise.js'
 import Kandinsky from '../models/kandinsky.js'
 import SadTalker from '../models/sadtalker.js'
 import ReplicateModel from '../lib/replicate-model.js'
@@ -6,23 +6,24 @@ import ReplicateModel from '../lib/replicate-model.js'
 class TalkingHead extends ReplicateModel {
   constructor (replicate, defaultInputs = {}) {
     super(replicate, defaultInputs)
-    this.bark = new Bark(this.replicate, {
-      history_prompt: 'announcer'
-    }, this.outputDirectory)
-    this.kandinsky = new Kandinsky(this.replicate, {}, this.outputDirectory)
+    this.tortoise = new Tortoise(this.replicate, {
+      voice_a: 'william'
+    })
+    this.kandinsky = new Kandinsky(this.replicate)
     this.sadTalker = new SadTalker(this.replicate, {
       enhancer: 'gfpgan',
-      preprocess: 'full',
-      still: true
+      preprocess: 'full'
     }, this.outputDirectory)
   }
 
-  async predict (input) {
-    const barkOutput = await this.bark.predict({ prompt: input.prompt })
-    const kandinskyOutput = await this.kandinsky.predict({ prompt: input.image_prompt })
+  async predict(input) {
+    const tortoisePromise = this.tortoise.predict({ text: input.prompt })
+    const kandinskyPromise = this.kandinsky.predict({ prompt: input.image_prompt || 'a portrait photo of a man' })
+    const [tortoiseOutput, kandinskyOutput] = await Promise.all([tortoisePromise, kandinskyPromise])
+
     const sadTalkerInput = {
       source_image: kandinskyOutput,
-      driven_audio: barkOutput
+      driven_audio: tortoiseOutput
     }
 
     return await this.sadTalker.predict(sadTalkerInput)
